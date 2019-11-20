09f22cdfdff6ffd88d4595afdb26d7872c8a3193
marcosrrg9813 <marcosrrg9813@gmail.com>
2019-11-19 23:02:26 -0400

    Change profileSrc in home-page

--- src/pages/home-page/HomePage.jsx
+++ src/pages/home-page/HomePage.jsx
@@ -1,29 +1,28 @@
-import React from 'react'
-import './HomePage.scss'
+import React from "react";
+import "./HomePage.scss";
 
-import UserDetails from '../../components/user-details/UserDetails'
-import CardMobile from '../../components/card-mobile/CardMobile'
+import UserDetails from "../../components/user-details/UserDetails";
+import CardMobile from "../../components/card-mobile/CardMobile";
 
 const HomePage = ({ userDetails }) => {
-
-  console.log(userDetails)
+  console.log(userDetails);
   let user = {
     id: userDetails.id,
     name: `${userDetails.firstName} ${userDetails.lastName}`,
-    profileSrc: require('../../assets/images/professor.jpeg'),
+    profileSrc: "https://i.ibb.co/GdyX9VY/frames.jpg",
     program: userDetails.program
-  }
+  };
 
   return (
-    <div className='home-page'>
-      <div className='home-page-mobile'>
+    <div className="home-page">
+      <div className="home-page-mobile">
         <CardMobile>
           <UserDetails user={user} />
         </CardMobile>
       </div>
       Inicio
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
