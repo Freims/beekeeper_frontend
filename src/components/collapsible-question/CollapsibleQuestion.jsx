1a98c66418e9654a1a8a9e2b480800b1d54bd6a5
leanthonyml <leanthonymll@gmail.com>
2020-04-05 13:45:32 -0400

    Add up icon to collapsible

--- src/components/collapsible-question/CollapsibleQuestion.jsx
+++ src/components/collapsible-question/CollapsibleQuestion.jsx
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "../help-page/HelpPage.scss";
 import Collapsible from "react-collapsible";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
+import { faChevronDown, faChevronUp } from "@fortawesome/free-solid-svg-icons";
 
 const CollapsibleQuestion = ({ title, description }) => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <Collapsible
+      onOpening={() => setIsOpen(true)}
+      onClosing={() => setIsOpen(false)}
       trigger={
         <div style={{ display: "flex", justifyContent: "space-between" }}>
-          {title} <FontAwesomeIcon icon={faChevronDown}></FontAwesomeIcon>
+          {title}
+          {isOpen ? (
+            <FontAwesomeIcon icon={faChevronUp}> </FontAwesomeIcon>
+          ) : (
+            <FontAwesomeIcon icon={faChevronDown}></FontAwesomeIcon>
+          )}
         </div>
       }
     >
