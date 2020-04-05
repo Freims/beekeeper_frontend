1554b92ca072694e9c04e0e0028d6d1031f88946
leanthonyml <leanthonymll@gmail.com>
2020-04-04 20:40:12 -0400

    Add exception to professorId NaN

--- src/components/user-details/UserDetails.jsx
+++ src/components/user-details/UserDetails.jsx
@@ -1,26 +1,26 @@
-import React from 'react'
-import './UserDetails.scss'
-import CircleAvatar from '../circle-avatar/CircleAvatar'
-import { connect } from 'react-redux'
+import React from "react";
+import "./UserDetails.scss";
+import CircleAvatar from "../circle-avatar/CircleAvatar";
+import { connect } from "react-redux";
 
 const UserDetails = ({ currentUser }) => {
-  const { name, id, program, img } = currentUser
-  
+  const { name, id, program, img } = currentUser;
+
   return (
-    <div className='user-details'>
-      <div className='circle-avatar-container'>
+    <div className="user-details">
+      <div className="circle-avatar-container">
         <CircleAvatar src={img} />
       </div>
-      <div className='info'>
-        <div className='name'>{name}</div>
-        <div className='id'>{id}</div>
-        <div className='program'>{program}</div>
+      <div className="info">
+        <div className="name">{name}</div>
+        {!isNaN(Number(id)) && <div className="id">{id}</div>}
+        <div className="program">{program}</div>
       </div>
     </div>
-  )
-}
-const mapStateToProps = state => ({
-  currentUser: state.user.currentUser
-})
+  );
+};
+const mapStateToProps = (state) => ({
+  currentUser: state.user.currentUser,
+});
 
-export default connect(mapStateToProps)(UserDetails)
+export default connect(mapStateToProps)(UserDetails);
